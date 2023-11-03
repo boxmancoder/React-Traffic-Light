f83d92731b3d569792bbb71c486cc8d7909c7805
Isaac F <114119376+boxmancoder@users.noreply.github.com>
2023-11-03 03:18:26 +0000

    completed to my best ability

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 //import your own components
 import Home from "./component/home.jsx";
 
