d16d3bef90cd3cba19bb97c0559d9080a31c0a98
Isaac F <114119376+boxmancoder@users.noreply.github.com>
2023-11-03 03:15:06 +0000

    made updates

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 
-
 const TrafficLight = () => {
     const [selectedColor, setSelectedColor] = useState('');
+    const [showPurple, setShowPurple] = useState(false);
+    const colors = ['red', 'yellow', 'green', 'purple']; // added purple to the array
+
+const cycleColor = () => {
+    const currentIndex = colors.indexOf(selectedColor);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    setSelectedColor(colors[nextIndex]);
+};
+
+const resetTrafficLight = () => {
+    setShowPurple(false);
+    setSelectedColor(''); // No color is selected initially
+};
 
     const handleClick = (color) => {
         setSelectedColor(color);
     };
 
+    const addPurpleLight = () => {
+        setShowPurple(true);
+    };
+
     return (
+    <div>
         <div className="traffic-light">
             <div
                 className={`light red ${selectedColor === 'red' ? 'selected' : ''}`}
@@ -22,8 +39,20 @@ const TrafficLight = () => {
                 className={`light green ${selectedColor === 'green' ? 'selected' : ''}`}
                 onClick={() => handleClick('green')}
             ></div>
+            {
+                showPurple && (
+                    <div
+                        className={`light purple ${selectedColor === 'purple' ? 'selected' : ''}`}
+                        onClick={() => handleClick('purple')}
+                    ></div>
+                )
+            }
         </div>
-    );
+        <button onClick={cycleColor}>Change Light</button>
+        {!showPurple && <button onClick={addPurpleLight}>Add Purple Light</button>}
+        {showPurple && <button onClick={resetTrafficLight}>Reset Traffic Light</button>}
+    </div>
+);
 };
 
 export default TrafficLight;
